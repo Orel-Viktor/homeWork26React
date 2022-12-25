a0449a83d1cc7a6661c22dd5e2f433abc3528627
Orel-Viktor <OrelViktor93@gmail.com>
2022-12-25 14:36:32 +0300

    home-work27

--- src/Card.js
+++ src/Card.js
@@ -1,33 +1,66 @@
 import React from "react";
 
-
-export function Card({ userName, lastname, technologies }) {
-  return (
-    <div className="card-inner">
-      <div className="card-inner__username">Name: {userName}</div>
-      <div className="card-inner__username">Last Name:{lastname}</div>
-      <div className="card-inner__technologies"> Learned Technologies :
-        {technologies.map((tech) => (
-          <div className="card-inner__technologies-item"  key={tech}>{tech}</div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-
-//  export class Card extends React.Component {
-//     render(){
-//         return (
-//             <div className="card-inner">
-//               <div className="card-inner__username">Name: {this.props.userName}</div>
-//               <div className="card-inner__lastname">Last Name:{this.props.lastname}</div>
-//               <div className="card-inner__technologies">Learned Technologies :
-//                 {this.props.technologies.map((tech) => (
-//                   <div className="card-inner__technologies-item" key={tech}>{tech}</div>
-//                 ))}
-//               </div>
-//             </div>
-//           );
-//     }
+// export function Card({ userName, lastname, technologies }) {
+//   return (
+//     <div className="card-inner">
+//       <div className="card-inner__username">Name: {userName}</div>
+//       <div className="card-inner__username">Last Name:{lastname}</div>
+//       <div className="card-inner__technologies"> Learned Technologies :
+//         {technologies.map((tech) => (
+//           <div className="card-inner__technologies-item"  key={tech}>{tech}</div>
+//         ))}
+//       </div>
+//     </div>
+//   );
 // }
+
+export class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      technologies: props.technologies,
+    };
+  }
+
+  addItem = (event) => {
+    event.preventDefault();
+    this.setState(
+      {
+        technologies: [
+          ...this.state.technologies,
+          event.target[0].value,
+        ],
+      },
+      () => {
+        event.target[0].value = "";
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="card-inner">
+        <div className="card-inner__username">Name: {this.props.userName}</div>
+        <div className="card-inner__lastname">
+          Last Name:{this.props.lastname}
+        </div>
+        <form
+          action="#"
+          className="card-inner__form js--form"
+          onSubmit={this.addItem}
+        >
+          <input type="text" className="js--form"></input>
+          <button>добавить</button>
+        </form>
+        <div className="card-inner__technologies">
+          Learned Technologies :
+          {this.state.technologies.map((tech,i) => (
+            <div className="card-inner__technologies-item" key={tech + i}>
+              {tech}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
