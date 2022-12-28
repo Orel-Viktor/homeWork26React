3b10eb448befe1ac450b8ccd6f85b0d9127845d7
Orel-Viktor <OrelViktor93@gmail.com>
2022-12-28 20:47:13 +0300

    homework28

--- src/Card.js
+++ src/Card.js
@@ -1,58 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
-
-// export class Card extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       technologies: props.technologies,
-//     };
-//     console.log(...this.state.technologies)
-//     console.log(props.technologies)
-//   }
-
-//   addItem = (event) => {
-//     event.preventDefault();
-//     this.setState(
-//       {
-//         technologies: [
-//           ...this.state.technologies,
-//           event.target[0].value,
-//         ],
-//       },
-//       () => {
-//         event.target[0].value = "";
-//       }
-//     );
-//   };
-
-//   render() {
-//     return (
-//       <div className="card-inner">
-//         <div className="card-inner__username">Name: {this.props.userName}</div>
-//         <div className="card-inner__lastname">
-//           Last Name:{this.props.lastname}
-//         </div>
-//         <form
-//           action="#"
-//           className="card-inner__form js--form"
-//           onSubmit={this.addItem}
-//         >
-//           <input type="text" className="js--form"></input>
-//           <button>добавить</button>
-//         </form>
-//         <div className="card-inner__technologies">
-//           Learned Technologies :
-//           {this.state.technologies.map((tech,i) => (
-//             <div className="card-inner__technologies-item" key={tech + i}>
-//               {tech}
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
+import React, { useState, useRef } from "react";
 
 export function Card(props) {
   const { userName, lastName } = props
@@ -60,17 +6,12 @@ export function Card(props) {
   const inputTechnologiesEL = useRef(null)
   const techItem = useRef(null)
 
-
   const addTechhologies = (event) => {
     event.preventDefault()
     const tech = inputTechnologiesEL.current.value
     setTechnologies([...technologies, tech])
     inputTechnologiesEL.current.value = ''
   }
-
-  useEffect(() => {
-    techItem.current.style.background = "blue"
-  })
 
 
   return (
